e578077404c4f2e5fb68f2d4c6bea586666aaec3
Ray <raysidarta@gmail.com>
2022-01-12 01:56:37 -0500

    Clean up code, add comments in my_change_notifier_classses
    Remove old commented out code, modify textSelection in clear sentence, and add explanatory code for TextEditingController, addWord and deleteWord

--- lib/letter_suggestion_files/my_change_notifier_classes.dart
+++ lib/letter_suggestion_files/my_change_notifier_classes.dart
@@ -21,63 +21,89 @@ class AppBrain with ChangeNotifier {
    unicode to properly delete an entire letter with deleteWord.
   */
   List<int> _numTChars = [];  // number of textdisplay characters
-  String _textDisplaySentence = "";
 
-
-
-
+  /* TextEditingController controls the TextField in the TextDisplay
+    widget.
+    * textDisplayController.text  = text of TextDisplay
+    * textDisplayController.selection = range of the highlighted text selection
+        This attribute's .baseOffset and .extentOffset generally are the indices
+        of where the selection begins and ends in the text.
+    When updated, it updates the TextDisplay.  It is modified by addWord,
+    deleteWord, and clearSentence.
+  */
   TextEditingController textDisplayController = TextEditingController();
-  // int cursorCharIndex = 0; // The true index for the cursor in the text string,
-  //since characters can be stacked and appear to have a smaller index.
 
 
 
+  //================================METHODS=====================================
 
-  //------METHODS-------
-
-  //ACCESSORS
+  //-------------------------------ACCESSORS-----------------------------------
   List<List<Offset>> getStrokeList() => _strokeList;
   List<String> getSuggestions()=> _suggestions;
   String getSuggestionAt(int index) => _suggestions[index];
   int getSuggestionsLength() => _suggestions.length;
   String getTextDisplaySentence() => textDisplayController.text;//_textDisplaySentence;
 
-  //MODIFIERS
+  List<int> getSelectionRange(){//get range of highlighted text in TextDisplay
+    //helper function of addWord and deleteWord
+    int leftIndex = textDisplayController.selection.baseOffset;
+    int rightIndex = textDisplayController.selection.extentOffset;
+    print('selection range: $leftIndex, $rightIndex');
+    leftIndex = leftIndex!=-1 ? leftIndex : 0;
+    rightIndex = rightIndex!=-1 ? rightIndex : 0;
+    return [leftIndex,rightIndex];
+  }
+
+  int getCursorDisplayIndex(int cursorCharIndex ){
+    //using the index position of a character in TextDisplay text, get the
+    // corresponding index of _numTChars.
+    //helper function for addWord and deleteWord
+    int s = 0;
+    int i = 0;
+    for ( ; s< cursorCharIndex && i< _numTChars.length ; i++ ){
+      s+= _numTChars[i];
+    }
+    return i;
+  }
 
 
 
-  void addWord (String aWord){
+
+  //---------------------------------MODIFIERS---------------------------------
+  void addWord (String aWord){//Inserts/replaces word into text of TextDisplay
     String displayText = textDisplayController.text;
-    // setCursorCharIndex(); // used to update the cursor index.
     List<int> selectionRange = getSelectionRange();
-    int cursorCharIndex = selectionRange[0];
-    int newCursorDisplayIndex = findCursorDisplayIndex(cursorCharIndex);
+    int cursorCharIndex = selectionRange[0]; //position in text
+    //position in _numTChars
+    int newCursorDisplayIndex = getCursorDisplayIndex(cursorCharIndex);
 
-    //If there is a selection of text to be replaced,
+    //Update text and _numTChar.
+    //If there is a highlighted selection of text to be replaced,
     if (selectionRange[0]!=selectionRange[1]){
       int lidx = selectionRange[0];
       int ridx = selectionRange[1];
-      int lDisplayIdx = findCursorDisplayIndex(lidx);
-      int rDisplayIdx = findCursorDisplayIndex(ridx);
+      int lDisplayIdx = getCursorDisplayIndex(lidx);
+      int rDisplayIdx = getCursorDisplayIndex(ridx);
 
       textDisplayController.text = displayText.substring(0,lidx) + aWord;
       if (ridx < displayText.length) {
-        textDisplayController.text += displayText.substring(ridx); //////////...
+        textDisplayController.text += displayText.substring(ridx);
       }
       _numTChars[lDisplayIdx] = aWord.length;
-      _numTChars = _numTChars.sublist(0, lDisplayIdx+1) + _numTChars.sublist(rDisplayIdx); /////....
-
+      _numTChars = _numTChars.sublist(0, lDisplayIdx+1) +
+          _numTChars.sublist(rDisplayIdx);
     }else {//Insert character at the cursor
       if (displayText.length == 0 || cursorCharIndex == displayText.length) {
         textDisplayController.text += aWord;
       } else {
-        textDisplayController.text = displayText.substring(0, cursorCharIndex) +
+        textDisplayController.text = displayText.substring(0, cursorCharIndex)+
             aWord + displayText.substring(cursorCharIndex);
       }
       _numTChars.insert(newCursorDisplayIndex, aWord.length);
     }
-    cursorCharIndex += aWord.length;
+
     //place the cursor in the correct position.
+    cursorCharIndex += aWord.length;
     textDisplayController.selection = TextSelection(
         baseOffset: cursorCharIndex, extentOffset: cursorCharIndex);
 
@@ -88,23 +114,28 @@ class AppBrain with ChangeNotifier {
 
     notifyListeners();
   }
-  void deleteWord(){
+
+
+  void deleteWord(){//Deletes words from TextDisplay.
     String displayText = textDisplayController.text;
     List<int> selectionRange = getSelectionRange();
     int lidx = selectionRange[0];
     int ridx = selectionRange[1];
+    //If text is nonempty and is a highlighted text selection or the cursor is
+    // in the middle of the text,
     if ((0<lidx || lidx<ridx) && displayText.length>0) {
-
-      int lDisplayIndex = findCursorDisplayIndex(lidx);
-      int rDisplayIndex = findCursorDisplayIndex(ridx);
-      if (lidx==ridx){
+      int lDisplayIndex = getCursorDisplayIndex(lidx);
+      int rDisplayIndex = getCursorDisplayIndex(ridx);
+      if (lidx==ridx){//If we are deleting behind the cursor:
         lidx-=_numTChars[lDisplayIndex-1];
         lDisplayIndex-=1;
       }
       textDisplayController.text =
           displayText.substring(0,lidx)+displayText.substring(ridx);
-      _numTChars = _numTChars.sublist(0, lDisplayIndex) + _numTChars.sublist(rDisplayIndex);
-      textDisplayController.selection = TextSelection(baseOffset : lidx, extentOffset: lidx);
+      _numTChars =
+          _numTChars.sublist(0,lDisplayIndex)+_numTChars.sublist(rDisplayIndex);
+      textDisplayController.selection =
+          TextSelection(baseOffset : lidx, extentOffset: lidx);
 
       print('Stats: ${lidx} ___ ${textDisplayController.selection.baseOffset} ${textDisplayController.selection.extentOffset} ${_numTChars}');
 
@@ -114,6 +145,8 @@ class AppBrain with ChangeNotifier {
   void clearSentence(){
     if (textDisplayController.text.length>0) {
       textDisplayController.text = '';
+      textDisplayController.selection =
+          TextSelection(baseOffset: 0,extentOffset:0);
       _numTChars.clear();
       notifyListeners();
     }
@@ -167,156 +200,9 @@ class AppBrain with ChangeNotifier {
   }
 
 
-  List<int> getSelectionRange(){
-    int leftIndex = textDisplayController.selection.baseOffset;
-    int rightIndex = textDisplayController.selection.extentOffset;
-    print('selection range: $leftIndex, $rightIndex');
-    leftIndex = leftIndex!=-1 ? leftIndex : 0;
-    rightIndex = rightIndex!=-1 ? rightIndex : 0;
-    return [leftIndex,rightIndex];
-  }
-
-
-  int findCursorDisplayIndex(int cursorCharIndex ){
-    //helper function for addWord and deleteWord
-    int s = 0;
-    int i = 0;
-    for ( ; s< cursorCharIndex && i< _numTChars.length ; i++ ){
-      s+= _numTChars[i];
-    }
-    return i;
-  }
-
 
   void printPathListString(){
     print(formatPathListToStr( strokeListToPathList(_strokeList)));
   }
 
 }
-
-
-
-//_____________________________________________________________________________-------------------------
-
-
-
-
-
-//
-//
-// class S111uggestionToDisplay with ChangeNotifier {
-//   //Class for the sentence displayed in the TextDisplay.
-//
-//   /*Since tibetan sentences are stacked, multiple adjacent UTF-16 characters are
-//    used to make 1 letter.  numTChars stores the length of each added chunk of
-//    unicode to properly delete an entire letter with deleteWord.
-//   */
-//   List<int> _numTChars = [];
-//   String _textDisplaySentence = "";
-//
-//
-//   String getTextDisplaySentence() => _textDisplaySentence;
-//
-//   void addWord (String aWord){
-//     _textDisplaySentence+=aWord;
-//     _numTChars.add(aWord.length);
-//     notifyListeners();
-//   }
-//   void clearSentence(){
-//     if (_textDisplaySentence.length>0) {
-//       _textDisplaySentence = "";
-//       _numTChars.clear();
-//       notifyListeners();
-//     }
-//   }
-//   void deleteWord(){
-//     if (_textDisplaySentence.length>0) {
-//       _textDisplaySentence = _textDisplaySentence.substring(0,
-//           _textDisplaySentence.length - _numTChars.last);
-//
-//       _numTChars.removeLast();
-//       notifyListeners();
-//     }
-//   }
-// }
-//
-//
-//
-// //_______________________________________________________________
-//
-//
-// class S111trokesToSuggestion with ChangeNotifier {
-//   List<List<Offset>> _strokeList = [];
-//   // List<String> TestSuggestions = "ཀ་ཏ་ད་ར་ཨ་པ་ཕ་ཆ་ཅ་ཇ་ས་ན་ཝ་ལ་ཐ་ས་ཤ་ཞ་ཉ་ང་མ་ཧ་ཧ་གུ་བོ་རི".split("་");
-//   List<String> _suggestions = [];
-//   final LetterEncyclopedia _encyclopedia = LetterEncyclopedia();
-//
-//
-//
-//   List<List<Offset>> getStrokeList() => _strokeList;
-//   List<String> getSuggestions()=> _suggestions;
-//   String getSuggestionAt(int index) => _suggestions[index];
-//   int getSuggestionsLength() => _suggestions.length;
-//
-//
-//
-//   void addStroke(List<Offset> aStroke) {
-//     _strokeList.add(aStroke);
-//     suggestLetters();
-//   }
-//   void deleteStroke() {
-//     if (_strokeList.length>0) {
-//       _strokeList.removeLast();
-//       suggestLetters();
-//     }
-//   }
-//
-//   void clearAllStrokesAndSuggestions() {
-//     _suggestions.clear();
-//     /*If suggestion bar is tapped, this will clear strokes.  This is also used
-//     in the undoslide button's onSlid function.*/
-//     _strokeList.clear();
-//
-//     notifyListeners();
-//   }
-//
-//
-//
-//   void _clearVerySmallStrokes(){
-//     /*remove the 1 or 2 point strokes from the strokeList, as they usually are
-//     errors. */
-//     for (int i = _strokeList.length-1 ; i>=0; i--){
-//       if (_strokeList[i].length <3){
-//         _strokeList.removeAt(i);
-//       }
-//     }
-//   }
-//
-//   void suggestLetters() {
-//     _clearVerySmallStrokes();
-//
-//     if (_strokeList.length >= 2) {
-//       tibetanLetterFinder(_strokeList, _suggestions, _encyclopedia );
-//
-//       // print("suggestLetter Strokelist: ${StrokeList.length} ");
-//       // Suggestions.shuffle();
-//       // Suggestions = Suggestions.reversed;
-//       // print("! $Suggestions !");
-//
-//     }else{
-//       _suggestions.clear();
-//     }
-//     notifyListeners();
-//   }
-//
-//
-//
-//   void printPathListString(){
-//     print(formatPathListToStr( strokeListToPathList(_strokeList)));
-//   }
-//
-// }
-
-
-
-
