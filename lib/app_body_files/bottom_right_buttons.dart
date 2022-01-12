cdb90dfa9891bd3be778d7899dd6e3861fd4b3dd
Ray <raysidarta@gmail.com>
2022-01-12 18:51:56 -0500

    Cleaned up bottom_right_buttons.dart file
    Deleted commented out code

--- lib/app_body_files/bottom_right_buttons.dart
+++ lib/app_body_files/bottom_right_buttons.dart
@@ -6,7 +6,6 @@ import 'package:flutter/services.dart';
 import '../styling_files/constants.dart';
 
 import '../styling_files/custom_slider_thumb_rect.dart';
-import '../styling_files/thumb_slider_delete_undo.dart';
 
 
 
@@ -50,19 +49,19 @@ class _TsegSheState extends State<TsegShe> {
 
           child: SliderTheme(
               data: SliderThemeData(
-                  thumbShape: CustomSliderThumbRect(
-                      min:0,
-                      max:1,
-                      thumbRadius: kRoundedButtonRadius,
-                      thumbHeight: kTsegSheButtonDim.dy ,//*1.1,
-                      thumbWidth:  kTsegSheButtonDim.dx,
-                      image: _image,
-
-                  ),
+                thumbShape: CustomSliderThumbRect(
+                  min:0,
+                  max:1,
+                  thumbRadius: kRoundedButtonRadius,
+                  thumbHeight: kTsegSheButtonDim.dy ,//*1.1,
+                  thumbWidth:  kTsegSheButtonDim.dx,
+                  image: _image,
+                ),
                 thumbColor: kTsegSheButtonColor,
                 // disabledThumbColor: Colors.blue,
                 inactiveTrackColor: Color(0x00),
                 activeTrackColor: Color(0x00),
+                // trackHeight: 15
 
               ),
               child: Slider(
@@ -130,82 +129,6 @@ class DeleteUndo extends StatelessWidget {
     );
   }
 }
-
-
-//
-// class DeleteUndo extends StatefulWidget {
-//
-//   final VoidCallback onPressed;
-//   final VoidCallback onSlid;
-//   DeleteUndo({Key key,
-//     @required this.onPressed,
-//     @required this.onSlid})
-//       :super(key: key);
-//
-//   @override
-//   _DeleteUndoState createState() => _DeleteUndoState();
-// }
-//
-// /// This is the private State class that goes with MyStatefulWidget.
-// class _DeleteUndoState extends State<DeleteUndo> {
-//   double restValue = 1;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Stack(
-//       alignment: Alignment.center,
-//       children: <Widget>[
-//         Container(),
-//         Container(
-//           color:Colors.black,
-//           width: 70,
-//           height: 90,
-//           child: SliderTheme(
-//             data: SliderThemeData(
-//                 thumbShape: ThumbSliderDeleteUndo(
-//                     min:0,
-//                     max:1,
-//                     thumbRadius: 15,
-//                     thumbHeight: 70,
-//                     text: "\u232B"
-//                 )
-//             ),
-//             child: Slider(
-//               value: restValue,
-//               min: 0,
-//               max: 1,
-//               onChanged: (double value) {
-//                 setState(() {
-//                   restValue=value;
-//                 });
-//               },
-//
-//               onChangeStart:(double startValue){
-//                 widget.onPressed();
-//               },
-//
-//               onChangeEnd:(double endValue){
-//                 if (endValue == 0){// hopefully this doesnt need a tolerance
-//                   widget.onSlid();
-//                 }
-//                 setState((){
-//                   restValue=1;
-//                 });
-//               },
-//             ),
-//           ),
-//         ),
-//
-//
-//
-//
-//
-//       ],
-//     );
-//   }
-// }
-
-
-
 
 
 
