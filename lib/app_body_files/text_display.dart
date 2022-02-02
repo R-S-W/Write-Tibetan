0e716afa15ee011ef9b7f1028010494557771547
Ray <raysidarta@gmail.com>
2022-02-01 23:38:36 -0500

    Restyle button row in TextDisplay
    Created a CustomPaint and a CustomPainter called ButtonRowBackgroundPainter to paint behind the row of buttons in text_display

--- lib/app_body_files/text_display.dart
+++ lib/app_body_files/text_display.dart
@@ -59,98 +59,108 @@ class _TextDisplayState extends State<TextDisplay> {
               ),
             ),
           ),
-          Container(
-            child: Row(//Copy, Delete Buttons
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                TextDisplayButton.left(//COPY BUTTON
-                  label: 'Copy\nAll',
-                  onPressed: (){
-                    AppBrain appBrain = Provider.of<AppBrain>(context, listen:false);
-                    FlutterClipboard.copy(appBrain.getTextDisplaySentence());
-                  },
-                ),
-
-
-                TextDisplayButton(//PASTE BUTTON
-                    label: 'Paste',
-                    onPressed: (){
-                      AppBrain appBrain = Provider.of<AppBrain>(context, listen: false);
-                      FlutterClipboard.paste().then(
-                        (pasteText) => appBrain.addWord(pasteText)
-                      );
-                    }
-                ),
-
-
-                Expanded(//Spacer
-                  child: Stack(
-                    children: <Widget>[
-                      Container(
-                        color:kTRed,
-                        height:60
-                      ),
-                      Container(
-                        height:60,
-                        decoration: BoxDecoration(
-                          color:kTWhite,
-                          borderRadius: BorderRadius.only(
-                            bottomLeft:Radius.elliptical(15,20),
-                            bottomRight: Radius.elliptical(15,20)
-                          ),
-                        ),
-                      )
-                    ]
-                  )
-                ),
-
-
-                TextDisplayButton(//UNDO BUTTON
-                  label: 'Undo',
-                  onPressed: (){
-                    AppBrain appBrain = Provider.of<AppBrain>(context, listen: false);
-                    appBrain.undo();
-                  }
-                ),
-
-                TextDisplayButton(//REDO BUTTON
-                    label: 'Redo',
-                    onPressed: (){
-                      AppBrain appBrain = Provider.of<AppBrain>(context, listen: false);
-                      appBrain.redo();
-                    }
-                ),
-
-
-                Container( //DELETE BUTTON
-                  width: 80,
-                  height: 60,
-                  child: ElevatedButton(
-                    child: Text(
-                      "Delete",
-                      style: kTextDisplayButtonTextStyle,
-                      softWrap: false,
-                      overflow:TextOverflow.visible,
+          ClipRect(
+            child: CustomPaint(
+              painter: ButtonRowBackgroundPainter(),
+              child: Container(
+                child: Row(//Copy, Delete Buttons
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    TextDisplayButton.left(//COPY BUTTON
+                      label: 'Copy\nAll',
+                      onPressed: (){
+                        AppBrain appBrain = Provider.of<AppBrain>(context, listen:false);
+                        FlutterClipboard.copy(appBrain.getTextDisplaySentence());
+                      },
                     ),
-                    onPressed: (){
-                      AppBrain appBrain = Provider.of<AppBrain>(context, listen:false);
-                      appBrain.deleteWord();
-                    },
-                    style:  ElevatedButton.styleFrom(
-                      primary: kCopyButtonColor,
-                      shape:RoundedRectangleBorder(
-                        borderRadius:  BorderRadius.only(
-                          topLeft: Radius.circular(1.5 * kRoundedButtonRadius)
+
+
+                    TextDisplayButton(//PASTE BUTTON
+                        label: 'Paste',
+                        onPressed: (){
+                          AppBrain appBrain = Provider.of<AppBrain>(context, listen: false);
+                          FlutterClipboard.paste().then(
+                            (pasteText) => appBrain.addWord(pasteText)
+                          );
+                        }
+                    ),
+
+
+                    TextDisplayButton(//UNDO BUTTON
+                      label: 'Undo',
+                      onPressed: (){
+                        AppBrain appBrain = Provider.of<AppBrain>(context, listen: false);
+                        appBrain.undo();
+                      }
+                    ),
+
+                    TextDisplayButton(//REDO BUTTON
+                        label: 'Redo',
+                        onPressed: (){
+                          AppBrain appBrain = Provider.of<AppBrain>(context, listen: false);
+                          appBrain.redo();
+                        }
+                    ),
+
+
+                    Container( //DELETE BUTTON
+                      width: 80,
+                      height: 60,
+                      child: ElevatedButton(
+                        child: Text(
+                          "Delete",
+                          style: kTextDisplayButtonTextStyle,
+                          softWrap: false,
+                          overflow:TextOverflow.visible,
                         ),
-                      )
-                    )
-                  ),
+                        onPressed: (){
+                          AppBrain appBrain = Provider.of<AppBrain>(context, listen:false);
+                          appBrain.deleteWord();
+                        },
+                        style:  ElevatedButton.styleFrom(
+                          primary: kCopyButtonColor,
+                          shape:RoundedRectangleBorder(
+                            borderRadius:  BorderRadius.only(
+                              topLeft: Radius.circular(1.5 * kRoundedButtonRadius)
+                            ),
+                          )
+                        )
+                      ),
+                    ),
+                  ]
                 ),
-              ]
+              ),
             ),
           ),
         ]
       ),
     );
   }
+}
+
+
+
+class ButtonRowBackgroundPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size){
+    final paintSettings1 = Paint()
+      ..color = kCopyButtonColor ;
+    final paintSettings2 = Paint()
+      ..blendMode = BlendMode.clear;
+      // ..color = Colors.white;
+    Rect canvasRectangle = Rect.fromLTRB(0, 0, size.width, size.height);
+    double h = size.height*1.3;
+    Rect ovalRectangle = Rect.fromCenter(
+        center: Offset(size.width/2,0),
+        width: size.width,
+        height: h);
+    canvas.saveLayer(canvasRectangle, Paint());
+    canvas.drawRect(canvasRectangle, paintSettings1);
+    canvas.drawOval(ovalRectangle, paintSettings2);
+    canvas.restore();
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+
 }
