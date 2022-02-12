055e22e221fd89d55891552a2282e54cf7fa01b4
Ray <raysidarta@gmail.com>
2022-02-11 21:51:17 -0500

    Improve drawing pad so curves are smoother
    Created a class, BezierInterpolationND that is used to take the position points in every stroke and interpolate them with cubic splines.  The Writing pad takes the functions and draws them.  The interpolation function is a bit unstable, and required a quick fix.  Each stroke is split up into smaller strokes that are interpolated.

--- lib/styling_files/custom_painters.dart
+++ lib/styling_files/custom_painters.dart
@@ -31,54 +31,72 @@ class StrokePainter extends CustomPainter{
       ..strokeWidth = 3.0;
 
 
-
+    // Path characterPath = Path();
+    print('strokeList len:  ${strokeList.length}');
     for (int i=0; i< strokeList.length; i++) {//For every stroke in strokeList,
+      print('iteration $i');
       for (int j =0; j< strokeList[i].length-1; j++){
-        canvas.drawLine(strokeList[i][j], strokeList[i][j+1], paintSettings);
+        // canvas.drawLine(strokeList[i][j], strokeList[i][j+1], paintSettings);
+      }
+      // canvas.drawPoints(PointMode.points, strokeList[i], paintSettings);
+
+      if (strokeList[i].length==0){
+        print('c');
+        continue;
       }
 
 
-      // Path strokePath = Path(); //Stroke data
-      // strokePath.moveTo(strokeList[i].first.dx, strokeList[i].first.dy);
-      // int numInterpolations = (strokeList[i].length/maxBezPoints).ceil();
-      //
-      // //draw endpoints
-      // canvas.drawPoints(
-      //     PointMode.points,
-      //     [strokeList[i].first, strokeList[i].last],
-      //     paintSettings);
-      //
-      // // Break stroke into sections, interpolate each section with
-      // // BezierInterpolation, and add the cubic interpolation function of that
-      // // section to the strokePath.
-      // for (int a= 1; a<=numInterpolations; a++){
-      //   List<List<double>> strokeSlice = []; //Small portion of strokeList
-      //   int sliceStart = (a-1)*maxBezPoints;
-      //   int sliceEnd = min(sliceStart + maxBezPoints, strokeList.length);
-      //   for (int jj = sliceStart; jj < sliceEnd; jj++) {
-      //     strokeSlice.add([strokeList[i][jj].dx, strokeList[i][jj].dy]);
-      //   }
-      //   print('a: $a,  strokeSlice: ${strokeSlice.length}, $strokeSlice,   $numInterpolations,  #strokelist: ${strokeList.length}');
-      //   if (strokeSlice.length < 3){// If portion too small for interpolation,
-      //     for (int j=0; j< strokeSlice.length-1; j++){//Connect the points
-      //       strokePath.lineTo(strokeList[i][j+1].dx,strokeList[i][j+1].dy);
-      //     }
-      //   }else{
-      //     //interpolate strokeSlice
-      //     BezierInterpolationND bezier = BezierInterpolationND(strokeSlice);
-      //     List<List<Offset>> controlPoints = bezier.computeControlPointsND();
-      //     // add interpolation to StrokePath
-      //     for (int j = 0; j<strokeSlice.length; j++){
-      //       strokePath.cubicTo(
-      //           controlPoints[j][0].dx, controlPoints[j][1].dx,
-      //           controlPoints[j][0].dy, controlPoints[j][1].dy,
-      //           strokeList[i][j + 1].dx, strokeList[i][j + 1].dy
-      //       );
-      //     }
-      //   }
-      // }
-      // canvas.drawPath(strokePath, paintSettings2);
+      Path strokePath = Path(); //Stroke data
+      strokePath.moveTo(strokeList[i].first.dx, strokeList[i].first.dy);
+      int numInterpolations = (strokeList[i].length/maxBezPoints).ceil();
+
+      //draw endpoints
+      canvas.drawPoints(
+          PointMode.points,
+          [strokeList[i].first, strokeList[i].last],
+          paintSettings);
+
+      // Break stroke into sections, interpolate each section with
+      // BezierInterpolation, and add the cubic interpolation function of that
+      // section to the strokePath.
+      for (int a= 1; a<=numInterpolations; a++){
+        List<List<double>> strokeSlice = []; //Small portion of strokeList
+        int sliceStart = (a-1)*maxBezPoints;
+        int sliceEnd = min(sliceStart + maxBezPoints, strokeList[i].length);
+        for (int jj = sliceStart; jj < sliceEnd; jj++) {
+          strokeSlice.add([strokeList[i][jj].dx, strokeList[i][jj].dy]);
+        }
+        print('a: $a,  strokeSlice: ${strokeSlice.length}, ,   $numInterpolations,  #strokelist: ${strokeList.length}');
+        print('        sliceStart: $sliceStart, sliceEnd: $sliceEnd');
+
+        if (a>1){ //If the full stroke is segmented,
+          //draw a line from previous stroke slice to current stroke slice
+          strokePath.lineTo(strokeSlice.first[0], strokeSlice.first[1]);
+        }
+
+        if (strokeSlice.length < 3){// If portion too small for interpolation,
+          for (int j=1; j< strokeSlice.length; j++){//Connect the points
+            print('lineto');
+            strokePath.lineTo(strokeSlice[j][0],strokeSlice[j][1]);
+          }
+        }else{
+          //interpolate strokeSlice
+          BezierInterpolationND bezier = BezierInterpolationND(strokeSlice);
+          List<List<Offset>> controlPoints = bezier.computeControlPointsND();
+          // add interpolation to StrokePath
+          for (int j = 0; j<strokeSlice.length-1; j++){
+            strokePath.cubicTo(
+              controlPoints[j][0].dx, controlPoints[j][1].dx,
+              controlPoints[j][0].dy, controlPoints[j][1].dy,
+              strokeSlice[j+1][0], strokeSlice[j+1][1]
+            );
+          }
+        }
+      }
+      // characterPath.addPath(strokePath, strokeList[i].first);
+      canvas.drawPath(strokePath, paintSettings2);
     }
+    // canvas.drawPath(characterPath, paintSettings2);
   }
 
   @override
