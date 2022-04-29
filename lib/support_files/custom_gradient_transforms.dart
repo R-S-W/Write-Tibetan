9ca1c9c0cdcdf8ee936e89bf8d7633f34c8bc9f3
Ray <raysidarta@gmail.com>
2022-04-28 23:53:42 -0400

    Add comments to custom_gradient_transforms.dart

--- lib/support_files/custom_gradient_transforms.dart
+++ lib/support_files/custom_gradient_transforms.dart
@@ -3,6 +3,18 @@ import 'dart:math' as m;
 //used in animations of the step by step strokes in the CharacterPage widgets in
 //Practice Mode
 class GradientRotationReverse extends GradientTransform{
+  /* To reverse the sweep angle, the starting angle alpha, the ending angle beta,
+    and the center of the angle must be known.
+    Reversing the angle requires one to invert the sweeping angle on the y-axis
+    and rotate it back to its original position counterclockwise by an angular
+    value.  That angular value is the sum of the alpha and beta.
+
+    (Extra)
+    Note that these transformations require the center of the sweeping angle to
+    be at the origin of your coordinate system.  The .translate() lines in transform()
+    ensure that the transformations are performed with the sweeping center as
+    the origin.
+  */
   double alpha = 0;
   double beta = 2*m.pi;
   Alignment center = Alignment(0.0,0.0);
@@ -11,12 +23,12 @@ class GradientRotationReverse extends GradientTransform{
   @override
   Matrix4 transform(Rect bounds, {TextDirection textDirection}){
     assert(bounds!=null);
-    final double angle = -alpha-beta;
 
-    final double sinRadians = m.sin(angle);
-    final double oneMinusCosRadians = 1 - m.cos(angle);
     Offset boxcenter = bounds.center;
     boxcenter+= Offset(boxcenter.dx*this.center.x,boxcenter.dy*this.center.y);
+    final double angle = -alpha-beta;
+    final double sinRadians = m.sin(angle);
+    final double oneMinusCosRadians = 1 - m.cos(angle);
     final double originX = sinRadians * boxcenter.dy + oneMinusCosRadians * boxcenter.dx;
     final double originY = -sinRadians * boxcenter.dx + oneMinusCosRadians * boxcenter.dy;
 
